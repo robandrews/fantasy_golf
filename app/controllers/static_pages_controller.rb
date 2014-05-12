4a566afe6cc4f9641381255a1d73f37ba986a815
Rob Andrews <rob.andrews@alumni.stanford.edu>
2014-05-12 14:52:38 -0700

    landing page tweaks

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   before_action :authenticate_user!, :only => [:bylaws]
   def welcome
+    redirect_to user_url(current_user) if user_signed_in?
   end
   
   def bylaws
